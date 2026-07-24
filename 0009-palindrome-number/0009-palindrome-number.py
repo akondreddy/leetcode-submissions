988b6478fe9a000867d59b3cf7712a38b891186a
Ashwin Kondreddy <ashwin.kondreddy@gmail.com>
2026-07-24 14:35:21 -0400

    0009-PALINDROME-NUMBER Stats: Time: 8 ms (71.57%), Space: 12.5 MB (17.39%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -3,14 +3,10 @@ class Solution(object):
         # Idea: Build the number in reverse
         # then check if the numbers equal each other
 
-        # Negative
-        if x < 0: 
-            return False
-
         original = x
         reverse = 0
-        while original > 0:
-            reverse = (reverse * 10) + (original % 10)
-            original = original / 10
-        return reverse == x
+        while x > 0:
+            reverse = (reverse * 10) + (x % 10)
+            x = x / 10
+        return reverse == original
         
