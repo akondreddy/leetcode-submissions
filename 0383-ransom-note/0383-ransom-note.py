17b1acb11678d5b7e46869e5b362773b1e3a59d4
Ashwin Kondreddy <ashwin.kondreddy@gmail.com>
2026-07-24 18:35:14 -0400

    0383-RANSOM-NOTE Stats: Time: 4 ms (98.32%), Space: 12.6 MB (87.24%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,6 +1,10 @@
 class Solution(object):
     def canConstruct(self, ransomNote, magazine):
-        # 
+        # Iterate through every char in ransomNote.
+        # .count(char) returns the amount of char in
+        # the string. If the ransomNote contains more of one
+        # character than magazine does, then it is not possible
+        # for magazine to create ransomNote, returning False.
         for char in set(ransomNote):
             if ransomNote.count(char) > magazine.count(char):
                 return False
