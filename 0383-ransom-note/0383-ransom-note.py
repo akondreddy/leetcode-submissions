4640395e816267e1ffc469b0a1604d80c83f4a38
Ashwin Kondreddy <ashwin.kondreddy@gmail.com>
2026-07-24 18:32:39 -0400

    0383-RANSOM-NOTE Stats: Time: 7 ms (96.08%), Space: 12.7 MB (54.3%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,18 +1,9 @@
 class Solution(object):
     def canConstruct(self, ransomNote, magazine):
-        letters = {}
-        # Parse through magazine and record letters
-        for i in magazine:
-            letters[i] = 1 + letters.get(i, 0)
-        
-        # Parse through ransomeNote, if letters contains
-        # the letter, then decrement the count of that letter.
-        # Otherwise, return False. If for loop terminates,
-        # return True
-        for i in ransomNote:
-            if i not in letters or letters[i] <= 0:
+        # 
+        for char in set(ransomNote):
+            if ransomNote.count(char) > magazine.count(char):
                 return False
-            letters[i] = letters[i] - 1
         return True
         
             
