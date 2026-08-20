903bc6b975acc925d2ebeb37ffa35e9f6d43d0c4
Ashwin Kondreddy <ashwin.kondreddy@gmail.com>
2026-08-20 13:28:24 -0400

    0287-FIND-THE-DUPLICATE-NUMBER Stats: Time: 36 ms (58.33%), Space: 20.9 MB (43.55%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,12 +1,22 @@
 class Solution(object):
     def findDuplicate(self, nums):
-         Brute force, iterate through array with 
-        # hashset
-        hashset = set()
-        for i in range(len(nums)):
-            if nums[i] in hashset:
-                return nums[i]
-            else:
-                hashset.add(nums[i])
-        return -1
+        # Solution 2: Use slow and fast pointer to find
+        # a cycle
+
+        slow, fast = 0, 0
+        while True:
+            # Traverse to next node
+            slow = nums[slow]
+            # Traverse two nodes
+            fast = nums[nums[fast]]
+            # Simply tells we are in a cycle
+            if slow == fast:
+                break
+        slow2 = 0
+        # The pointers will meet at the cycle entrance
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow
         
