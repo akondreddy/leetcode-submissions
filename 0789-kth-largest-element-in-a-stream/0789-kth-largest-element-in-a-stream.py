ae48d2ca27ddafb286021e46c5ce632d5bfb4d2d
Ashwin Kondreddy <ashwin.kondreddy@gmail.com>
2026-09-05 18:16:31 -0400

    0789-KTH-LARGEST-ELEMENT-IN-A-STREAM Stats: Time: 16 ms (62.64%), Space: 25.3 MB (95.66%) - LeetHub

--- 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.py
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.py
@@ -1,13 +1,19 @@
 class KthLargest:
-    # First solution
-    # Every time a new element is added,
-    # sort the list then just pick the len - k
-    # index for the largest
+    # Use a min heap
+    # Add to the heap, if the number of elements
+    # in the heap exceeds k, then remove elements.
+    # This way, the element that we return will be 
+    # the kth largest integer
     def __init__(self, k: int, nums: List[int]):
+        self.minHeap = nums
         self.k = k
-        self.nums = nums
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
 
     def add(self, val: int) -> int:
-        self.nums.append(val)
-        self.nums = sorted(self.nums)
-        return self.nums[len(self.nums) - self.k]
+        heapq.heappush(self.minHeap, val)
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap) 
+        return self.minHeap[0]
+        
